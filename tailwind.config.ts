cc387931c0be9619e527010abb3f059fd7811514
Jessie Odeh <jessieodeh08@gmail.com>
2024-03-14 17:36:03 +0100

    init repo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+ darkMode: 'class',
+ content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+ "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+ "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+ theme: {
+  extend: {},
+ },
+ plugins: [
+  plugin(function ({ addBase, theme }) {
+   addBase({
+    h1: { fontSize: '32px', lineHeight: '44.8px' },
+    h2: { fontSize: '20px' },
+    h3: { fontSize: '18px' },
+   });
+  }),
+ ],
 };
 export default config;
