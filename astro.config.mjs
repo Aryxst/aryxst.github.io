c9f9f67fe330c00a478caebd54387a0c20058019
Jessie Odeh <jessieodeh08@gmail.com>
2024-04-21 16:05:04 +0200

    - add basic component variants

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import remarkToc from 'remark-toc';
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
- devToolbar: { enabled: false },
- integrations: [tailwind(), sitemap(), mdx()],
- site: 'https://aryxst.github.io/',
- server: {
-  port: 3000,
- },
+  devToolbar: {
+    enabled: false
+  },
+  integrations: [tailwind(), sitemap(), mdx()],
+  site: 'https://aryxst.github.io/',
+  server: {
+    port: 3000
+  },
+  markdown: {
+    remarkPlugins: [remarkToc]
+  }
 });
