8c9d61b988aa33928bae3411f5da7107c7d9ddc0
Jessie Odeh <jessieodeh08@gmail.com>
2024-04-21 17:31:04 +0200

    - add 2 remark plugins - remove unused remark plugin - generic refactoring - configure shiki highlighter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,25 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import remarkToc from 'remark-toc';
+import remarkCodeImport from 'remark-code-import';
+import remarkCodeTitle from 'remark-code-title';
+import mdx from '@astrojs/mdx';
 
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false
+ devToolbar: {
+  enabled: false,
+ },
+ integrations: [tailwind(), sitemap(), mdx()],
+ site: 'https://aryxst.github.io/',
+ server: {
+  port: 3000,
+ },
+ markdown: {
+  remarkPlugins: [remarkToc, remarkCodeImport, remarkCodeTitle],
+  gfm: true,
+  syntaxHighlight: 'shiki',
+  shikiConfig: {
+   wrap: false,
   },
-  integrations: [tailwind(), sitemap(), mdx()],
-  site: 'https://aryxst.github.io/',
-  server: {
-    port: 3000
-  },
-  markdown: {
-    remarkPlugins: [remarkToc]
-  }
+ },
 });
