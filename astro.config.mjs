9c5f4f18c1b29f1d9367d5c18b879332fe156685
Jessie Odeh <jessieodeh08@gmail.com>
2024-04-22 14:51:23 +0200

    - add uses section(hidden) - add card and cardgroup components - rename ContentContainer to Container across all files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,38 @@ import remarkToc from 'remark-toc';
 import remarkCodeImport from 'remark-code-import';
 import remarkCodeTitle from 'remark-code-title';
 import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
 
+// https://astro.build/config
 export default defineConfig({
  devToolbar: {
   enabled: false,
  },
- integrations: [tailwind(), sitemap(), mdx()],
+ integrations: [
+  tailwind(),
+  sitemap(),
+  mdx(),
+  icon({
+   svgoOptions: {
+    multipass: true,
+    plugins: [
+     {
+      name: 'preset-default',
+      params: {
+       overrides: {
+        // customize default plugin options
+        inlineStyles: {
+         onlyMatchedOnce: false,
+        },
+        // or disable plugins
+        removeDoctype: false,
+       },
+      },
+     },
+    ],
+   },
+  }),
+ ],
  site: 'https://aryxst.github.io/',
  server: {
   port: 3000,
